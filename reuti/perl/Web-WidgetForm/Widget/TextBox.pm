8d9c20df4fdf97b61fce42958e6d94d1691cefc8
zoso <zoso>
2004-04-20 10:00:29 +0000

    - Cambiada la herencia a «Web::Widget::BaseInput» - Fijado el valor del argumento «type» a «text» - Borrados dos métodos, que ahora se cogen implícitamente del padre

--- reuti/perl/Web-WidgetForm/Widget/TextBox.pm
+++ reuti/perl/Web-WidgetForm/Widget/TextBox.pm
@@ -2,39 +2,15 @@ package Web::Widget::TextBox;
 
 use strict;
 
-use base qw(Web::Widget::JavascriptComponent);
+use base qw(Web::Widget::BaseInput);
 
 sub new {
    my ($class, @args) = @_;
 
    my $self = $class->SUPER::new(@args);
-   push @{$self->{HTML_VALID_ATTRS}}, 'type', 'value', 'readonly', 'tabindex',
-                                      'accesskey', 'size', 'maxlength';
+   push @{$self->{VALUE_HTML_ATTRS}}, 'size', 'maxlength';
+   $self->arg('type', 'text');      # Fixed value
    return $self;
-}
-
-sub setup_form {
-   my ($self, @args) = @_;
-
-   $self->SUPER::setup_form(@args);
-   my ($form, $name, $args) = ($self->{FORM}, $self->{NAME}, $self->{ARGS});
-
-   # Common form rules
-   $args->{focus} && $form->add_prop('init', "\%$name\%.focus();");
-   $args->{nonempty} && $form->add_prop('before_send', "if (\%$name\%.value.match(/^ *\$/)) { alert('".($args->{nonempty_msg} || "Error: empty field. Please fill in.")."'); \%$name\%.focus(); return false; };");
-   $args->{before_send_extra} && $form->add_prop('before_send', $args->{before_send_extra});
-}
-
-sub render {
-   my ($self, $extra_args) = @_;
-
-   $self->SUPER::render;
-   my ($name,         $value,           $extra_attrs) =
-      ($self->{NAME}, $self->get_value, $self->get_html_attrs);
-   $value ||= "";
-   return <<EOWIDGET;
-   <input type="text" name="$name" value="$value" $extra_attrs>
-EOWIDGET
 }
 
 1;
