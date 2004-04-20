de816d42bb5f1b4255a02a366df5f13a176c1e63
zoso <zoso>
2004-04-20 09:49:52 +0000

    - Sobrecargado el constructor, para añadir a cada objeto dos atributos HTML   más

--- reuti/perl/Web-WidgetForm/t/Web/Widget/Test.pm
+++ reuti/perl/Web-WidgetForm/t/Web/Widget/Test.pm
@@ -2,6 +2,15 @@ package Web::Widget::Test;
 
 use base qw(Web::Widget);
 
+sub new {
+   my ($self, @args) = @_;
+
+   $self = $self->SUPER::new(@args);
+   push @{$self->{EMPTY_HTML_ATTRS}}, 'checked';
+   push @{$self->{VALUE_HTML_ATTRS}}, 'size';
+   return $self;
+}
+
 sub type_data_transform {
    my ($self, $values) = @_;
    $values->{type_Test}++;
