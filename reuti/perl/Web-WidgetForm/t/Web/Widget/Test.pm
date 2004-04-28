d2b93d2f0ed2c3e826702f1f8572c075181d8de3
zoso <zoso>
2004-04-28 08:28:20 +0000

    - Añadido el atributo «onchange» a la lista de atributos con valor - Sobrecargado el método «get_calc_html_attrs»

--- reuti/perl/Web-WidgetForm/t/Web/Widget/Test.pm
+++ reuti/perl/Web-WidgetForm/t/Web/Widget/Test.pm
@@ -7,13 +7,20 @@ sub new {
 
    $self = $self->SUPER::new(@args);
    push @{$self->{EMPTY_HTML_ATTRS}}, 'checked';
-   push @{$self->{VALUE_HTML_ATTRS}}, 'size';
+   push @{$self->{VALUE_HTML_ATTRS}}, 'size', 'onchange';
    return $self;
 }
 
 sub type_data_transform {
    my ($self, $values) = @_;
    $values->{type_Test}++;
+}
+
+sub get_calc_html_attrs {
+   my ($self, $args) = @_;
+   my %base_attrs = $self->SUPER::get_calc_html_attrs($args);
+   $base_attrs{onchange} .= $self->get_form->get_js_name($self->get_name).".value = ''; ";
+   return (%base_attrs, i_dont_exist_in_html_attrs => 'no, really');
 }
 
 sub widget_data_transform {
