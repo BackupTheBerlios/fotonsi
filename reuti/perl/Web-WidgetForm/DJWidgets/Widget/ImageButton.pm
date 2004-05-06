3f858bd203105f1eb9bf8406a187f9da9b394937
zoso <zoso>
2004-05-06 09:11:56 +0000

    Componente para los <input type="image">.

---
+++ reuti/perl/Web-WidgetForm/DJWidgets/Widget/ImageButton.pm
@@ -0,0 +1,18 @@
+package Web::DJWidgets::Widget::ImageButton;
+
+use strict;
+
+use base qw(Web::DJWidgets::Widget::Button);
+
+sub new {
+   my ($class, @args) = @_;
+
+   my $self = $class->SUPER::new(@args);
+   push @{$self->{VALUE_HTML_ATTRS}}, 'src';
+   # Default value (check actual arguments passed to this method, not the
+   # value received from the 'Button' widget type)
+   $self->arg('type', 'image') unless defined $args[2]->{'type'};
+   return $self;
+}
+
+1;
