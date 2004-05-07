e03b5111cc9c4ff8b3d1fc01068e72d98af0b53d
zoso <zoso>
2004-05-07 09:50:04 +0000

    - Añadida la propiedad «alt»

--- reuti/perl/Web-WidgetForm/DJWidgets/Widget/ImageButton.pm
+++ reuti/perl/Web-WidgetForm/DJWidgets/Widget/ImageButton.pm
@@ -8,7 +8,7 @@ sub new {
    my ($class, @args) = @_;
 
    my $self = $class->SUPER::new(@args);
-   push @{$self->{VALUE_HTML_ATTRS}}, 'src';
+   push @{$self->{VALUE_HTML_ATTRS}}, 'src', 'alt';
    # Default value (check actual arguments passed to this method, not the
    # value received from the 'Button' widget type)
    $self->arg('type', 'image') unless defined $args[2]->{'type'};
