f7b92fb01de74c55cdb1bb88833abf9939728826
zoso <zoso>
2004-09-06 18:15:45 +0000

    - Convertido el fichero a latin1 - Añadido el campo obs_cosa

--- reuti/perl/Class-DBI-FastPager/t/paging.sql
+++ reuti/perl/Class-DBI-FastPager/t/paging.sql
@@ -1,16 +1,17 @@
 CREATE TABLE tcosa (
   cod_cosa int,
   des_cosa varchar(50),
+  obs_cosa varchar(255),
   PRIMARY KEY(cod_cosa)
 );
 
-INSERT INTO tcosa VALUES (1, 'Primera cosa');
-INSERT INTO tcosa VALUES (2, 'Segunda cosa');
-INSERT INTO tcosa VALUES (3, 'Tercera cosa');
-INSERT INTO tcosa VALUES (4, 'Cuarta cosa');
-INSERT INTO tcosa VALUES (5, 'Quinta cosa');
-INSERT INTO tcosa VALUES (6, 'Sexta cosa');
-INSERT INTO tcosa VALUES (7, 'SÃ©ptima cosa');
-INSERT INTO tcosa VALUES (8, 'Octava cosa');
-INSERT INTO tcosa VALUES (9, 'Novena cosa');
-INSERT INTO tcosa VALUES (10, 'DÃ©cima cosa');
+INSERT INTO tcosa VALUES (1, 'Primera cosa', '');
+INSERT INTO tcosa VALUES (2, 'Segunda cosa', '');
+INSERT INTO tcosa VALUES (3, 'Tercera cosa', NULL);
+INSERT INTO tcosa VALUES (4, 'Cuarta cosa', '');
+INSERT INTO tcosa VALUES (5, 'Quinta cosa', NULL);
+INSERT INTO tcosa VALUES (6, 'Sexta cosa', '');
+INSERT INTO tcosa VALUES (7, 'Séptima cosa', NULL);
+INSERT INTO tcosa VALUES (8, 'Octava cosa', NULL);
+INSERT INTO tcosa VALUES (9, 'Novena cosa', '');
+INSERT INTO tcosa VALUES (10, 'Décima cosa', NULL);
