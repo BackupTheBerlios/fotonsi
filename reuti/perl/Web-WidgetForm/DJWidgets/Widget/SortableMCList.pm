45d263c44f59827b6218ef98db88eb2d13f288b6
zoso <zoso>
2005-04-18 19:30:05 +0000

    - Se reciben dos nuevos atributos, «asc_order_img» y «desc_order_img», para   especificar imágenes de flechas de ordenación

--- reuti/perl/Web-WidgetForm/DJWidgets/Widget/SortableMCList.pm
+++ reuti/perl/Web-WidgetForm/DJWidgets/Widget/SortableMCList.pm
@@ -11,11 +11,13 @@ sub render {
     my $sort_idx = ($sort_col < 0 ? -$sort_col : $sort_col)-1;
     my $sort_key = $self->arg('cols')->[$sort_idx]->{key};
     my $name = $self->get_name;
+    my ($asc_order_img, $desc_order_img) = ($args->{asc_order_img},
+                                            $args->{desc_order_img});
 
     my @new_cols = @{$args->{cols}};
     my $form_name = $self->get_form->get_name;
     for (my $i = 0; $i <= $#new_cols; ++$i) {
-        $new_cols[$i]->{html_title} ||= "<a href=\"#\" onclick=\"document.$form_name.$name\_sort.value = ".(($i+1) * ($i+1 == $sort_col ? -1 : 1))."; document.$form_name.submit(); return false\">$new_cols[$i]->{title}</a>";
+        $new_cols[$i]->{html_title} ||= "<a href=\"#\" onclick=\"document.$form_name.$name\_sort.value = ".(($i+1) * ($i+1 == $sort_col ? -1 : 1))."; document.$form_name.submit(); return false\">$new_cols[$i]->{title} ".($sort_idx == $i && $asc_order_img ? "<img border=\"0\" ".($i+1 == $sort_col ? "alt=\"v\" src=\"$asc_order_img\"" : "alt=\"^\" src=\"$desc_order_img\"").">" : "")."</a>";
     }
 
     my $sort_sub = $sort_col > 0 ? sub { $a->{$sort_key} cmp $b->{$sort_key} } :
