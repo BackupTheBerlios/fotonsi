02342bc6ad42173355d899a444057f1c0e5127f2
zoso <zoso>
2005-05-04 22:43:39 +0000

    - Más traducciones a inglés - Añadidas comprobaciones para dar mensajes de error «claros» cuando no se   pasan los parámetros adecuados al componente

--- reuti/perl/Web-WidgetForm/DJWidgets/Widget/MultiColList.pm
+++ reuti/perl/Web-WidgetForm/DJWidgets/Widget/MultiColList.pm
@@ -7,33 +7,40 @@ sub render {
     my ($self, $args) = @_;
     $args ||= { $self->get_args };
 
-    my $cabecera = join("\n", map {
-                                    if ($_->{html_title}) {
-                                        "<td>$_->{html_title}</td>"
-                                    }
-                                    elsif ($_->{title_url} || $_->{title_js}) {
-                                        my $url = $_->{title_url} || '#';
-                                        "<td><a ".($_->{title_js} ? "onclick=\"$_->{title_js}\"" : "")." href=\"$url\">$_->{title}</a></td>"
-                                    }
-                                    else {
-                                        "<td>$_->{title}</td>"
-                                    }
+    my $cnt = 0;
+    my $header = join("\n", map {
+                                  $cnt++;
+                                  if ($_->{html_title}) {
+                                      "<td>$_->{html_title}</td>"
                                   }
-                                  @{$args->{'cols'}});
+                                  elsif ($_->{title_url} || $_->{title_js}) {
+                                      my $url = $_->{title_url} || '#';
+                                      "<td><a ".($_->{title_js} ? "onclick=\"$_->{title_js}\"" : "")." href=\"$url\">$_->{title}</a></td>"
+                                  }
+                                  else {
+                                      defined $_->{title} || die "Can't find title for column #$cnt";
+                                      "<td>$_->{title}</td>"
+                                  }
+                                }
+                                @{$args->{'cols'}});
 
     my $data_row_attrs = $self->get_html_attrs($args->{'data_row_attrs'} || {},
                                                ['class']);
 
-    my $datos = join("\n", map {
-                                   my $row = $_;
-                                   "<tr $data_row_attrs>\n".
-                                   (join("\n",
-                                         map {
-                                             defined $_->{closure} ? "<td>".$_->{closure}->($row)."</td>" : "<td>$row->{$_->{key}}</td>"
-                                         } @{$args->{'cols'}})).
-                                   "\n</tr>"
-                               }
-                               @{$args->{'data'}});
+    $cnt = 0;
+    my $data = join("\n", map {
+                                  $cnt++;
+                                  my $row = $_;
+                                  "<tr $data_row_attrs>\n".
+                                  (join("\n",
+                                        map {
+                                            defined $_->{closure} ||
+                                                    defined $_->{key} || die "Can't retrieve value for column $cnt (define either 'key' or 'closure')";
+                                            defined $_->{closure} ? "<td>".$_->{closure}->($row)."</td>" : "<td>$row->{$_->{key}}</td>"
+                                        } @{$args->{'cols'}})).
+                                  "\n</tr>"
+                              }
+                              @{$args->{'data'}});
 
     my $table_attrs = $self->get_html_attrs($args->{'table_attrs'} || {},
                                             ['class']);
@@ -43,9 +50,9 @@ sub render {
     return <<EOW;
     <table $table_attrs>
         <tr $title_row_attrs>
-            $cabecera
+            $header
         </tr>
-        $datos
+        $data
     </table>
 EOW
 }
