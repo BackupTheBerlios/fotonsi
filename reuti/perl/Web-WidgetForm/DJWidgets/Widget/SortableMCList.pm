2e68dfbbf15c32bbbc27072edefd6425b73ceaca
zoso <zoso>
2005-05-04 22:42:31 +0000

    - Comprobados los argumentos para dar errores «claros»

--- reuti/perl/Web-WidgetForm/DJWidgets/Widget/SortableMCList.pm
+++ reuti/perl/Web-WidgetForm/DJWidgets/Widget/SortableMCList.pm
@@ -9,6 +9,7 @@ sub render {
 
     my $sort_col = $self->_get_sort_col;
     my $sort_idx = ($sort_col < 0 ? -$sort_col : $sort_col)-1;
+    ref $self->arg('cols') eq 'ARRAY' || die "No columns defined (incorrect 'cols' argument)";
     my $sort_key = $self->arg('cols')->[$sort_idx]->{key};
     my $name = $self->get_name;
     my ($asc_order_img, $desc_order_img) = ($args->{asc_order_img},
@@ -17,6 +18,8 @@ sub render {
     my @new_cols = @{$args->{cols}};
     my $form_name = $self->get_form->get_name;
     for (my $i = 0; $i <= $#new_cols; ++$i) {
+        defined $new_cols[$i]->{html_title} ||
+                defined $new_cols[$i]->{title} || die "No title for column ".($i+1);
         $new_cols[$i]->{html_title} ||= "<a href=\"#\" onclick=\"document.$form_name.$name\_sort.value = ".(($i+1) * ($i+1 == $sort_col ? -1 : 1))."; document.$form_name.submit(); return false\">$new_cols[$i]->{title} ".($sort_idx == $i && $asc_order_img ? "<img border=\"0\" ".($i+1 == $sort_col ? "alt=\"v\" src=\"$asc_order_img\"" : "alt=\"^\" src=\"$desc_order_img\"").">" : "")."</a>";
     }
 
