8f820b21921b1b758b9dfce5c78cef624daab541
zoso <zoso>
2005-06-09 12:15:59 +0000

    - Se «parametriza» el nombre del fichero de variables, para que se pueda tener   uno por instalación - Cargadas las definiciones de SQL y bases de datos del fichero   «sql_defaults.mk» (por ahora, de una ruta fija que no es la definitiva) - Implementación inicial de «install_sql» y de «install_db» - Se hace más flexible la definición de la variable «INSTALLATION_ID» - Reorganización menor

--- reuti/Makefile/mason-webapp.mk
+++ reuti/Makefile/mason-webapp.mk
@@ -2,40 +2,52 @@
 
 # VARIABLE DEFINITION ========================================================
 
-VARS_FILE ?= vars
-include $(VARS_FILE)
-
-# Mandatory variables --------------------------------------------------------
+# Application id
 ifndef APPLICATION_ID
 $(error The APPLICATION_ID variable is mandatory)
 endif
+# Installation id (you can use a file named "default_install" to set the
+# default installation, or pass the INSTALLATION_ID as a Makefile variable. If
+# not, it default to the hostname)
+INSTALLATION_ID       ?= $(shell if [ -r default_install ]; then cat default_install; else hostname; fi)
 
-ifndef APACHE_CONF_DIR
-$(error The APACHE_CONF_DIR variable is mandatory)
+# Find out a suitable variables file
+VARS_FILE ?= $(shell if [ -r vars-$(INSTALLATION_ID) ]; then echo vars-$(INSTALLATION_ID); else echo vars; fi)
+ifeq ($(strip $(shell [ -r $(VARS_FILE) ] && echo -n $(VARS_FILE))),)
+$(error No se encuentra el fichero de variables para la instalación $(INSTALLATION_ID) ('$(VARS_FILE)'))
 endif
+# Actually include it
+include $(VARS_FILE)
+
+# Included Makefile snippets -------------------------------------------------
+include /home/zoso/src/foton/reuti/Makefile/sql_defaults.mk
 
 # Optional variables ---------------------------------------------------------
 TEMPLATE_PROCESSOR    ?= process_conf_template
-INSTALLATION_ID       ?= $(shell hostname)
 # Important project subdirs
 MODULES_DIR           ?= perl
 MASON_DIR             ?= mason
 SQL_DIR               ?= sql
 # Installation files/dirs
+APACHE_CONF_DIR       ?= /etc/apache/conf.d
 MAIN_APACHE_CONF_FILE ?= foton-mason-$(APPLICATION_ID)-$(INSTALLATION_ID).conf
 APP_CONF_FILE         ?= $(APPLICATION_ID)-$(INSTALLATION_ID).ini
 APP_CONF_DIR          ?= /etc/foton
 INSTALLATION_DIR      ?= /var/www/mason/$(APPLICATION_ID)
 EMPTY_DIRS            ?= sesiones
+# Database/SQL related
+INSTALLATION_SQL_FILES ?= $(SQL_DIR)/*.sql_install
+SQL_FILES              ?= $(SQL_DIR)/*.sql
 # Misc. installation info
-WEB_USER              ?= $(shell if getent passwd www-data; then echo www-data; else echo apache; fi)
-WEB_GROUP             ?= $(shell if getent group www-data; then echo www-data; else echo apache; fi)
+WEB_USER  ?= $(shell if getent passwd www-data; then echo www-data; else echo apache; fi)
+WEB_GROUP ?= $(shell if getent group www-data; then echo www-data; else echo apache; fi)
 
 
 # GENERIC RULES ==============================================================
 # "Compile" files from templates and the vars file
 %: %.in $(VARS_FILE)
 	$(TEMPLATE_PROCESSOR) $< $(VARS_FILE) >$@
+
 
 # INSTALLATION RULES =========================================================
 
@@ -55,11 +67,17 @@ $(APP_CONF_DIR)/$(APP_CONF_FILE): $(APPLICATION_ID).ini
 
 
 # Database creation ----------------------------------------------------------
-install_db::
+install_db:: $(addsuffix .sql_install_output,$(wildcard $(INSTALLATION_SQL_FILES)))
+
+%.sql_install_output: %
+	$(call load_sql_file,$<,$(DB_NAME),$(DB_USER),$(DB_PASSWORD),$(DB_HOST),$(DB_PORT),$@);
 
 
 # SQL installation -----------------------------------------------------------
-install_sql::
+install_sql:: $(addsuffix .sql_output,$(wildcard $(SQL_FILES)))
+
+%.sql_output: %
+	$(call load_sql_file,$<,$(DB_NAME),$(DB_USER),$(DB_PASSWORD),$(DB_HOST),$(DB_PORT),$@);
 
 
 # Perl modules installation --------------------------------------------------
