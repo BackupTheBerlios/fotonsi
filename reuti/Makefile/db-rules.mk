3cfdd6e76f202521b2793b0abcc2e3431bcba900
setepo <setepo>
2005-07-26 22:25:10 +0000

    - Reglas para crear, borrar y cargar datos de la BBDD. De momento   sólo para postgresql

--- reuti/Makefile/db-rules.mk
+++ reuti/Makefile/db-rules.mk
@@ -8,6 +8,15 @@ install_db:: $(addsuffix .sql_install_output,$(wildcard $(INSTALLATION_SQL_FILES
 %.sql_install_output: %
 	$(call load_sql_file,$<,$(DB_NAME),$(DB_USER),$(DB_PASSWORD),$(DB_HOST),$(DB_PORT),$@);
 
+create_db::
+	dropdb $(if $(DB_HOST),-h $(DB_HOST),) $(if $(DB_PORT),-p $(DB_PORT),) -U $(DB_CREATOR_USER) $(DB_NAME) || true
+	createdb $(if $(DB_HOST),-h $(DB_HOST),) $(if $(DB_PORT),-p $(DB_PORT),) -U $(DB_CREATOR_USER) -E $(DB_ENCODING) -O $(DB_USER) $(DB_NAME)
+
+dump_db::
+	pg_dump -U $(DB_USER) $(if $(DB_HOST),-h $(DB_HOST),) $(if $(DB_PORT),-p $(DB_PORT),) -U $(DB_CREATOR_USER) -D --data-only $(DB_NAME) > $(DB_DUMP_DATA) || true
+
+upgrade_db:: dump_db create_db install_db
+
 
 # SQL installation -----------------------------------------------------------
 install_sql:: $(addsuffix .sql_output,$(wildcard $(SQL_FILES)))
@@ -16,4 +25,4 @@ install_sql:: $(addsuffix .sql_output,$(wildcard $(SQL_FILES)))
 	$(call load_sql_file,$<,$(DB_NAME),$(DB_USER),$(DB_PASSWORD),$(DB_HOST),$(DB_PORT),$@);
 
 
-.PHONY: install_db install_sql
+.PHONY: install_db install_sql create_db upgrade_db
