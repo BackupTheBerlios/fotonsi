defb1d34a03bdd294a99b44c118c3afaf39d6346
setepo <setepo>
2005-07-26 22:30:03 +0000

    - Añadir dependencias y movidas a las reglas del db-rules

--- reuti/Makefile/fos-rules.mk
+++ reuti/Makefile/fos-rules.mk
@@ -1,6 +1,4 @@
 
-include scf/db-rules.mk
-
 $(FOS_STRUCTURE):: $(FOS_SOURCE)
 	$(FOSC_BIN) $(FOS_SOURCE) --structure-only $(FOSC_PLUGIN) > $(FOS_STRUCTURE)
 
@@ -8,4 +6,15 @@ $(FOS_INTEGRITY):: $(FOS_SOURCE)
 	$(FOSC_BIN) $(FOS_SOURCE) --integrity-only $(FOSC_PLUGIN) > $(FOS_INTEGRITY)
 
 
+install_db:: $(FOS_STRUCTURE) $(FOS_INTEGRITY) 
 
+clean::
+	rm $(FOS_INTEGRITY) $(FOS_STRUCTURE)
+
+include scf/db-vars.mk
+
+INSTALLATION_SQL_FILES := $(FOS_STRUCTURE) $(INSTALLATION_SQL_FILES) $(FOS_INTEGRITY)
+
+include scf/db-rules.mk
+
+.PHONY: clean
