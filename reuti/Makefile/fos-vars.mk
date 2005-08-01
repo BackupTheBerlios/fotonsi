721a905b996acadbe5f42a379c283125c6f0b0a6
setepo <setepo>
2005-08-01 08:45:27 +0000

    - Cambiamos el sufijo de los ficheros SQL generados para que no el patrón   no se pise con los .sql normales normales

--- reuti/Makefile/fos-vars.mk
+++ reuti/Makefile/fos-vars.mk
@@ -6,7 +6,7 @@ FOSC_BIN 		?= fosc
 FOSC_PLUGIN		?= pg
 
 FOS_SOURCE		?= sql/$(APPLICATION_ID).fos
-FOS_STRUCTURE		?= sql/000-$(APPLICATION_ID)-fos-struct.sql
-FOS_INTEGRITY		?= sql/999-$(APPLICATION_ID)-fos-integrity.sql
+FOS_STRUCTURE		?= sql/000-$(APPLICATION_ID)-fos-struct.gensql
+FOS_INTEGRITY		?= sql/999-$(APPLICATION_ID)-fos-integrity.gensql
 
 INSTALLATION_SQL_FILES	:= $(FOS_STRUCTURE) $(INSTALLATION_SQL_FILES) $(FOS_INTEGRITY)
