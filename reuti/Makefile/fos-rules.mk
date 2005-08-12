33d4a5e006f57bd75f5e849ea94add3bb8b39e15
alberto <alberto>
2005-08-12 12:27:33 +0000

    Añadimos un target específico para sqlobject.

--- reuti/Makefile/fos-rules.mk
+++ reuti/Makefile/fos-rules.mk
@@ -5,6 +5,9 @@ $(FOS_STRUCTURE):: $(FOS_SOURCE)
 $(FOS_INTEGRITY):: $(FOS_SOURCE)
 	$(FOSC_BIN) $(FOS_SOURCE) --integrity-only $(FOSC_PLUGIN) > $(FOS_INTEGRITY)
 
+# SQLObject specific
+python/$(APPLICATION_ID)/fos_$(APPLICATION_ID)_tables.py: $(FOS_SOURCE)
+	 $(FOSC_BIN) $(FOS_SOURCE) sqlobject > $@
 
 install_db:: $(FOS_STRUCTURE) $(FOS_INTEGRITY) 
 
