019258f27b3f234673b35e5227e90e6140f9f203
alberto <alberto>
2005-08-12 13:42:24 +0000

    Cambiamos la ruta de python para que no se pise con el modulo de la aplicacion de webware.

--- reuti/Makefile/fos-rules.mk
+++ reuti/Makefile/fos-rules.mk
@@ -6,7 +6,7 @@ $(FOS_INTEGRITY):: $(FOS_SOURCE)
 	$(FOSC_BIN) $(FOS_SOURCE) --integrity-only $(FOSC_PLUGIN) > $(FOS_INTEGRITY)
 
 # SQLObject specific
-python/$(APPLICATION_ID)/fos_$(APPLICATION_ID)_tables.py: $(FOS_SOURCE)
+python/$(APPLICATION_ID)data/fos_$(APPLICATION_ID)_tables.py: $(FOS_SOURCE)
 	 $(FOSC_BIN) $(FOS_SOURCE) sqlobject > $@
 
 install_db:: $(FOS_STRUCTURE) $(FOS_INTEGRITY) 
